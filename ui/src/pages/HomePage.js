b21ef6f293f2da3594ed61535c80acdb9629d225
Andrew Lombardi <andrew@mysticcoders.com>
2020-03-06 07:25:26 -0800

    integrate plotly and the react items

--- ui/src/pages/HomePage.js
+++ ui/src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Plot from 'react-plotly.js';
+
 import { Title, Section, Content } from "rbx"
 
 import { HeaderContainer } from '../containers/HeaderContainer'
@@ -15,20 +17,20 @@ export const HomePage = () => {
                 <Content>
                     <Title>Home</Title>
 
-                    <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-                    took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-                    unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-                    including versions of Lorem Ipsum.
-                    </p>
+                    <Plot
+                        data={[
+                        {
+                            x: [1, 2, 3],
+                            y: [2, 6, 3],
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: {color: 'red'},
+                        },
+                        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                        ]}
+                        layout={ {width: 320, height: 240, title: 'Confirmed Cases United States'} }
+                    />
 
-                    <p>
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, 
-                    a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word 
-                    in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by 
-                    Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from 
-                    a line in section 1.10.32.
-                    </p>
                 </Content>
             </Section>
             
