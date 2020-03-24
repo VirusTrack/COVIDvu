8017b3440eb7a91cac100697c48132cc2a731da6
Lucas Marohn <lukemarohn@gmail.com>
2020-03-24 12:36:58 -0700

    increase size of log buttons

--- ui/src/components/GraphScaleControl.js
+++ ui/src/components/GraphScaleControl.js
@@ -11,9 +11,9 @@ export const GraphScaleControl = ({showLog, handleGraphScale, secondaryGraph}) =
     return (
         <Level>                        
             <Level.Item align="left">
-                Graph Scale:&nbsp;<Button color={!showLog?"primary":"default"} onClick={() => { if(showLog) { handleGraphScale(false) } }}>Linear</Button>
+                Graph Scale:&nbsp;<Button size="medium" color={!showLog?"primary":"default"} onClick={() => { if(showLog) { handleGraphScale(false) } }}>Linear</Button>
                 &nbsp;
-                <Button tooltip="Steeper slope on log scale means faster disease spread" color={showLog?"primary":"default"} onClick={() => { if(!showLog) { handleGraphScale(true) } }}>Logarithmic</Button>
+                <Button tooltip="Steeper slope on log scale means faster disease spread" size="medium" color={showLog?"primary":"default"} onClick={() => { if(!showLog) { handleGraphScale(true) } }}>Logarithmic</Button>
             </Level.Item>
         </Level>
     )
