21dac9417009a8503f86950880d70aa77109a545
Andrew Lombardi <andrew@mysticcoders.com>
2020-04-06 17:38:43 -0700

    simple test to start us off

---
+++ ui/src/components/BoxWithLoadingIndicator.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import '@testing-library/jest-dom/extend-expect'
+
+import BoxWithLoadingIndicator from './BoxWithLoadingIndicator'
+
+it('Box does not show if hasData', () => {
+    const { getByText } = render(<BoxWithLoadingIndicator />)
+
+    expect(getByText("Loading...")).toBeInTheDocument()
+})
+
+it('Box shows if hasData is true', () => {
+    const { getByText } = render(<BoxWithLoadingIndicator hasData={true}><h1>Has Data</h1></BoxWithLoadingIndicator>)
+
+    expect(getByText("Has Data")).toBeInTheDocument()
+})
