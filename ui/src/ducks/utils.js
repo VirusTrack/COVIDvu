0fed387f7de139ca87ee10d6473c8a93010f8278
Andrew Lombardi <andrew@mysticcoders.com>
2020-04-07 18:23:20 -0700

    add the utils for good measure

--- ui/src/ducks/utils.js
+++ ui/src/ducks/utils.js
@@ -1,46 +1,46 @@
 export const calculateMortality = (deaths, confirmed) => {
-  const mortality = {};
+  const mortality = {}
 
   if (deaths !== null && confirmed !== null) {
     for (const country of Object.keys(deaths)) {
       for (const date of Object.keys(deaths[country])) {
-        const deathAtDate = deaths[country][date];
+        const deathAtDate = deaths[country][date]
 
         if (!Object.prototype.hasOwnProperty.call(confirmed, country)) {
-          continue;
+          continue
         }
-        const confirmedAtDate = confirmed[country][date];
+        const confirmedAtDate = confirmed[country][date]
 
         if (!Object.prototype.hasOwnProperty.call(mortality, country)) {
-          mortality[country] = {};
+          mortality[country] = {}
         }
-        mortality[country][date] = deathAtDate / confirmedAtDate;
+        mortality[country][date] = deathAtDate / confirmedAtDate
       }
     }
   }
 
-  return { mortality };
-};
+  return { mortality }
+}
 
 export const extractLatestCounts = (stats, daysAgo = 0) => {
-  const regionWithLatestCounts = [];
+  const regionWithLatestCounts = []
 
   for (const region of Object.keys(stats)) {
-    const dates = Object.keys(stats[region]).sort();
+    const dates = Object.keys(stats[region]).sort()
 
-    const lastDate = dates[dates.length - daysAgo - 1];
+    const lastDate = dates[dates.length - daysAgo - 1]
 
-    const yesterDate = dates[dates.length - daysAgo - 2];
+    const yesterDate = dates[dates.length - daysAgo - 2]
 
-    const currentNumbers = stats[region][lastDate];
-    const yesterdayNumbers = stats[region][yesterDate];
+    const currentNumbers = stats[region][lastDate]
+    const yesterdayNumbers = stats[region][yesterDate]
 
     regionWithLatestCounts.push({
       region,
       stats: currentNumbers,
       dayChange: currentNumbers - yesterdayNumbers
-    });
+    })
   }
 
-  return regionWithLatestCounts;
-};
+  return regionWithLatestCounts
+}
