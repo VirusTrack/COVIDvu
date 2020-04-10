e2f90a22d0263b309f086baadd3b0c57a047af95
E Ciurana <pr3d4t0r@cime.net>
2020-04-10 07:29:45 +0000

    "0525 - Almost there - all bundles generated automatically"

--- work/test/covidvu/pipeline/test_vuregions.py
+++ work/test/covidvu/pipeline/test_vuregions.py
@@ -3,102 +3,35 @@
 # vim: set fileencoding=utf-8:
 
 
-from covidvu.pipeline.vudpatch import resolveReportFileName
-from covidvu.pipeline.vujson import SITE_DATA
-from covidvu.pipeline.vujson import parseCSSE
-from covidvu.pipeline.vuregions import DEFAULT_OUTPUT_JSON_FILE_NAME
-from covidvu.pipeline.vuregions import COUNTRIES_REGIONS
-from covidvu.pipeline.vuregions import RegionsAggregator
+from covidvu.pipeline.vuregions import main
 
 import json
 import os
-import re
 
 
 # --- constants ---
 
-TEST_SITE_DATA = './resources/test_pipeline'
+TEST_BUNDLE_OUTPUT_FILE_NAME = 'bundle-test-regions.json'
+TEST_MASTER_DATABASE         = os.path.join('./resources/test_databases', 'test-virustrack.db')
+TEST_SITE_DATA               = './resources/test_pipeline'
 
-TEST_JH_CSSE_PATH = os.path.join(os.getcwd(), 'resources', 'test_COVID-19','csse_covid_19_data','csse_covid_19_time_series')
-TEST_JH_CSSE_FILE_CONFIRMED    = os.path.join(TEST_JH_CSSE_PATH, 'time_series_covid19_confirmed_global.csv')
-TEST_JH_CSSE_FILE_DEATHS       = os.path.join(TEST_JH_CSSE_PATH, 'time_series_covid19_deaths_global.csv')
-TEST_JH_CSSE_FILE_CONFIRMED_US = os.path.join(TEST_JH_CSSE_PATH, 'time_series_covid19_confirmed_US.csv')
-TEST_JH_CSSE_FILE_DEATHS_US    = os.path.join(TEST_JH_CSSE_PATH, 'time_series_covid19_deaths_US.csv')
 
 # +++ tests +++
 
-expectedPath      = None
-regionsAggregator = None    # global object - used in all tests
+def test_main():
+    bundle, \
+    bundleFileName = main(TEST_MASTER_DATABASE, TEST_SITE_DATA, TEST_BUNDLE_OUTPUT_FILE_NAME)
 
+    assert bundle
+    assert isinstance(bundle, dict)
+    assert 'confirmed' in bundle
+    assert 'North America' in bundle['confirmed']
 
-def _purge(purgeDirectory, pattern):
-    for f in os.listdir(purgeDirectory):
-        if re.search(pattern, f):
-            os.remove(os.path.join(purgeDirectory, f))
+    assert os.path.exists(bundleFileName)
 
+    altBundle = json.load(open(bundleFileName, 'r'))
 
-def test_RegionsAggregator_object():
-    global regionsAggregator
+    assert bundle['confirmed'].keys() == altBundle['confirmed'].keys()
 
-    regionsAggregator = RegionsAggregator(siteData = TEST_SITE_DATA)
-
-    assert 'confirmed' in regionsAggregator.buckets
-    assert 'North America' in regionsAggregator.buckets['confirmed']
-    assert regionsAggregator.globalInputDatasets
-
-    assert 'confirmed' in regionsAggregator.globalInputDatasets
-    assert 'Region' in regionsAggregator.globalInputDatasets['confirmed'].index
-    dataset = regionsAggregator.globalInputDatasets['confirmed']
-    dataset = dataset[dataset.index == 'Region']
-    assert dataset['2020-03-15']['Region'] > 0
-
-    assert isinstance(regionsAggregator.globalDatasets, dict)
-
-    continentalRegions = regionsAggregator.buckets
-
-    assert 'confirmed' in continentalRegions
-    assert 'North America' in continentalRegions['confirmed']
-
-    assert isinstance(continentalRegions['confirmed']['Continental Region'], dict)
-    assert len(continentalRegions['confirmed']['Continental Region']) > 0
-
-
-def test_RegionsAggregator_getCanonicalOutputFileName():
-    global expectedPath
-
-    expectedPath = os.path.join(TEST_SITE_DATA, DEFAULT_OUTPUT_JSON_FILE_NAME)
-    assert expectedPath == regionsAggregator.getCanonicalOutputFileName()
-
-
-def test_RegionsAggregator_JSON():
-    regionsAggregator.toJSONFile()
-
-    assert os.path.exists(expectedPath)
-    os.unlink(expectedPath)
-
-
-def test_COUNTRIES_REGIONS_table():
-    testSiteData = os.path.join(os.getcwd(), 'resources', 'test_site_data')
-    officialCountriesFileName = resolveReportFileName(testSiteData, 'confirmed', '')
-
-    try:
-        _ =  parseCSSE('confirmed',
-              siteData=testSiteData,
-              jhCSSEFileConfirmed=TEST_JH_CSSE_FILE_CONFIRMED,
-              jhCSSEFileDeaths=TEST_JH_CSSE_FILE_DEATHS,
-              jhCSSEFileConfirmedUS=TEST_JH_CSSE_FILE_CONFIRMED_US,
-              jhCSSEFileDeathsUS=TEST_JH_CSSE_FILE_DEATHS_US,
-              )
-
-        countriesCSSE = json.load(open(officialCountriesFileName, 'r')).keys()
-
-        countriesCheck = [countries for countries in COUNTRIES_REGIONS.keys() if countries not in countriesCSSE ]
-
-        assert len(countriesCheck)
-        assert 'Other Region' in countriesCheck
-    except Exception as e:
-        raise e
-    finally:
-        _purge(testSiteData, '.json')
-
+    os.unlink(bundleFileName)
 
