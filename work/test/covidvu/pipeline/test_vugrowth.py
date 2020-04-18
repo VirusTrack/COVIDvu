e3d1820e1f6d6a330b955a82889fc4ed05dcf680
jaryaman <j.aryaman25@gmail.com>
2020-04-18 09:49:15 +0100

    Completed tests

--- work/test/covidvu/pipeline/test_vugrowth.py
+++ work/test/covidvu/pipeline/test_vugrowth.py
@@ -2,26 +2,57 @@
 # See: https://github.com/VirusTrack/COVIDvu/blob/master/LICENSE
 # vim: set fileencoding=utf-8:
 
+import os
 import pandas as pd
 from covidvu.cryostation import Cryostation
+from covidvu.config import MASTER_DATABASE
 from covidvu.pipeline.vugrowth import _computeGrowthFor
 from covidvu.pipeline.vugrowth import _getGrowthGaugeData
 from covidvu.pipeline.vugrowth import WINDOW_SIZE
 from covidvu.pipeline.vugrowth import _appendGrowthToCountries
+from covidvu.pipeline.vugrowth import computeGrowth
 
 from pandas.core.frame import DataFrame
 from pandas.core.series import Series
 
 REAL_DATABASE_PATH = './database/virustrack.db'
-TODAY_DATE = pd.to_datetime('2020-04-15').date()
+TEST_TODAY_DATE = pd.to_datetime('2020-04-15').date()
+TEMP_DATABASE_NAME = 'temp-virustrack.db'
+
+def makeTestDatabase():
+    with Cryostation(TEMP_DATABASE_NAME) as cryostationTest:
+        with Cryostation(MASTER_DATABASE) as cryostation:
+            unitedStates = cryostation['US']
+            california = {'confirmed': unitedStates['provinces']['California']['confirmed']}
+            newYork = {'confirmed': unitedStates['provinces']['New York']['confirmed']}
+            newJersey = {'confirmed': unitedStates['provinces']['New Jersey']['confirmed']}
+
+            item = {'confirmed': unitedStates['confirmed'],
+                    'provinces': {'California': california,
+                                  'New York': newYork,
+                                  'New Jersey': newJersey,
+                                  },
+                    'key': 'US'}
+
+            cryostationTest['US'] = item
+
+    with Cryostation(TEMP_DATABASE_NAME) as cryostationTest:
+        with Cryostation(MASTER_DATABASE) as cryostation:
+            italy = {'confirmed': cryostation['Italy']['confirmed'],
+                     'key': 'Italy'}
+            uk = {'confirmed': cryostation['United Kingdom']['confirmed'],
+                  'key': 'United Kingdom'}
+
+            cryostationTest['Italy'] = italy
+            cryostationTest['United Kingdom'] = uk
 
 
 def test__computeGrowthFor():
-    with Cryostation(REAL_DATABASE_PATH) as cryostation:
+    makeTestDatabase()
+    with Cryostation(TEMP_DATABASE_NAME) as cryostation:
         print('Loading time series for countries...')
         regions = cryostation.timeSeriesFor(regionType         = 'country',
                                             casesType          = 'confirmed',
-                                            nRegionsLimit      = 3,
                                             )
     growth = _computeGrowthFor(regions, WINDOW_SIZE)
     assert isinstance(growth, DataFrame)
@@ -38,7 +69,7 @@ def test__computeGrowthFor():
 
 def test__getGrowthGaugeData():
     growth = test__computeGrowthFor()
-    growthGaugeData = _getGrowthGaugeData(growth, TODAY_DATE)
+    growthGaugeData = _getGrowthGaugeData(growth, TEST_TODAY_DATE)
     assert 'yesterday' in growthGaugeData
     assert 'lastWeek' in growthGaugeData
     assert 'lastTwoWeek' in growthGaugeData
@@ -51,5 +82,24 @@ def test__getGrowthGaugeData():
     return growthGaugeData
 
 
-def test__growthGaugeData():
+def test__appendGrowthToCountries():
+    growthGaugeData = test__getGrowthGaugeData()
+    _appendGrowthToCountries(growthGaugeData, TEMP_DATABASE_NAME)
+    with Cryostation(TEMP_DATABASE_NAME) as cryostationTest:
+        assert isinstance(cryostationTest['US']['growth'], dict)
+        assert isinstance(cryostationTest['Italy']['growth'], dict)
+        assert isinstance(cryostationTest['United Kingdom']['growth'], dict)
+    os.remove(TEMP_DATABASE_NAME)
 
+
+def test_computeGrowth():
+    makeTestDatabase()
+    computeGrowth(regionType='country',
+                  casesType='confirmed',
+                  todayDate=TEST_TODAY_DATE,
+                  databasePath=TEMP_DATABASE_NAME)
+    with Cryostation(TEMP_DATABASE_NAME) as cryostationTest:
+        assert isinstance(cryostationTest['US']['growth'], dict)
+        assert isinstance(cryostationTest['Italy']['growth'], dict)
+        assert isinstance(cryostationTest['United Kingdom']['growth'], dict)
+    os.remove(TEMP_DATABASE_NAME)
